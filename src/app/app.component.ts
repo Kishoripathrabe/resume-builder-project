4045c5a7c4c9bc62b5110f176182da5d10bb8b84
“Kishoripathrabe” <kishoripathrabe123@gmail.com>
2023-01-22 20:46:03 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: `<div fxLayoutAlign="centre centre" fxLayout="column" fxLyoutGap="30px">
+  <mat-card>
+  
+  </mat-card>
+  </div>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'resume-builder';
+}
